bf471732c1437df65514c9ec28c558cac0fc837c
Seoyoon <wideskyinme@gmail.com>
2023-07-18 19:49:55 +0900

    init commit

---
+++ src/main/java/com/example/hangusemiproject/HanguSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangusemiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanguSemiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanguSemiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hangusemiproject/HanguSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangusemiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanguSemiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
