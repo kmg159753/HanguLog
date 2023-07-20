580e75a11870e664e39c73fee565c937fb83668d
kmg159753 <mingyugim584@gmail.com>
2023-07-20 09:47:58 +0900

    refacotr : 토큰 만료시 예외처리 추가

--- src/main/java/com/example/hangusemiproject/desk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hangusemiproject/desk/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.hangusemiproject.desk.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -39,6 +41,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(
                 customException,
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({ExpiredJwtException.class})
+    public ResponseEntity<CustomException> expiredJwtExceptionHandler(ExpiredJwtException ex) {
+        CustomException customException = new CustomException("fail", "Token expired", HttpStatus.UNAUTHORIZED.value());
+        return new ResponseEntity<>(
+                customException,
+                HttpStatus.UNAUTHORIZED
         );
     }
 
